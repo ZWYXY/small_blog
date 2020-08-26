89863c0f93e2edc2f6b50e1858383831382ec57b
醉卧意逍遥 <57378028+IloveChina77@users.noreply.github.com>
2020-08-26 13:17:44 +0800

    增加注解，校验参数

--- src/main/java/com/zr/blog/po/Type.java
+++ src/main/java/com/zr/blog/po/Type.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class Type {
     @Id
     @GeneratedValue
     private Long id;
+    @NotBlank(message = "分类名称不能为空")
     private String name;
 
     @OneToMany(mappedBy = "type")
